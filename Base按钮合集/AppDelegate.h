1bc89e2aefba0f5cde36d9bc452acd777a2c689e
wkaiyan@163.com <wkaiyan@163.com>
2016-09-11 15:47:26 +0800

    Initial Commit

---
+++ Base按钮合集/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Base按钮合集
+//
+//  Created by WKY on 16/9/11.
+//  Copyright © 2016年 WKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Base按钮合集Tests/Base____Tests.m
@@ -0,0 +1,39 @@
+//
+//  Base____Tests.m
+//  Base按钮合集Tests
+//
+//  Created by WKY on 16/9/11.
+//  Copyright © 2016年 WKY. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Base____Tests : XCTestCase
+
+@end
+
+@implementation Base____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
